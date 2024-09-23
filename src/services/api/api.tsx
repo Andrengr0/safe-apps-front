30e19c9ec953935474107eae97c14f33bceb11f8
André Negreiros <112659872+Andrengr0@users.noreply.github.com>
2024-09-23 01:51:14 -0300

    correcao

--- src/services/api/api.tsx
+++ src/services/api/api.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configuração básica do Axios
 const api = axios.create({
-  baseURL: 'https://api-safeapps.netlify.app/api', // Coloque aqui a URL do seu backend
+  baseURL: 'https://testewebsite.shop/api', // Coloque aqui a URL do seu backend
   headers: {
     'Content-Type': 'application/json',
   },
